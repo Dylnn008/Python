7b7ad36adccc6a9e5850640da8ef6507611a9c1d
Dylan08 <dylanalmaras72@gmail.com>
2026-01-23 17:06:21 -0300

    Creacion de un bucle para que el usuario decida si cifrar otra palabra o no

--- Cifrado_Cesar.py
+++ Cifrado_Cesar.py
@@ -9,3 +9,14 @@ def cifrar_cesar(texto, nume_de_desplazamiento):
     texto_cifrado = texto.translate(tabla_de_traduccion)
     return texto_cifrado
 print("Texto cifrado: ", cifrar_cesar("",""))
+
+#Hacer que la funcion se repita hasta que el usuario decida salir
+while True:
+    repetir = input("¿Desea cifrar otro texto? (s/n): ").lower()
+    if repetir == 's':
+        print("Texto cifrado: ", cifrar_cesar("",""))
+    elif repetir == 'n':
+        print("¡Hasta luego!")
+        break
+    else:
+        print("Opción no válida. Por favor, ingrese 's' o 'n'.")
